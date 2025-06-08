c83a4f63f8826831fe2abfb08d066950548cd349
Leon <132808925+Leon8326@users.noreply.github.com>
2025-06-08 22:51:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -128,5 +128,5 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-zenity --info --text="✅ Installation complete!\nTry running:  wcc yourfile.wh"
+zenity --info --text="Installation complete!\nTry running:  whee yourfile.wh"
 
