cc957b1137d2de267b42041db7f49722eea5f3a5
Leon <132808925+Leon8326@users.noreply.github.com>
2025-06-13 16:42:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -143,7 +143,7 @@ done
 # Final step: download syntax.json
 echo "100"
 echo "# Downloading syntax.json..."
-sudo curl -L "$SYNTAX_URL" -o "$INSTALL_DIR/syntax.json" >/dev/null 2>&1
+sudo curl -L --progress "$SYNTAX_URL" -o "$INSTALL_DIR/syntax.json"
 ) |
 zenity --progress \
   --title="Installing Whee Software" \
